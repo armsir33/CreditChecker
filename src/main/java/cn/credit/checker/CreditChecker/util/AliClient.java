b78c191d1054cd9a2b67726e969a38eda7ce5ce8
Ze Ni <ze.ni@worldline.com>
2018-09-21 15:32:01 +0200

    First commit to AliClient

--- src/main/java/cn/credit/checker/CreditChecker/util/AliClient.java
+++ src/main/java/cn/credit/checker/CreditChecker/util/AliClient.java
@@ -1,22 +1,25 @@
 package cn.credit.checker.CreditChecker.util;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.web.client.RestTemplate;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class AliClient {
-	
-	private final RestTemplate client;
-	
-	public AliClient(final RestTemplate client) {
-		this.client = client;
-	}
-	
-	public static AliClient create() {
-		final RestTemplate rt = new RestTemplate();
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-		final AliClient aliClient = new AliClient(rt);
-		return aliClient;
-	}
+
+    CloseableHttpClient httpclient = HttpClients.createDefault();
+
+    CloseableHttpResponse get(String url) {
+        HttpGet httpGet = new HttpGet(url);
+        try {
+            CloseableHttpResponse response = httpclient.execute(httpGet);
+            return response;
+        } catch (Exception e) {
+            log.error("Failed to call " + url);
+            throw new RuntimeException("Failed to call " + url, e);
+        }
+    }
 }
