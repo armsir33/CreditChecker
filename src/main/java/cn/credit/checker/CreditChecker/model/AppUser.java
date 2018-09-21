d07fdabd14524a5579e60114699adea99e77f8a8
jing <jing@jingdembp.lan>
2018-09-21 21:43:45 +0200

    Fix startup failure

--- src/main/java/cn/credit/checker/CreditChecker/model/AppUser.java
+++ src/main/java/cn/credit/checker/CreditChecker/model/AppUser.java
@@ -4,9 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-import lombok.Data;
-
-@Data
 @Entity
 public class AppUser {
 
@@ -15,7 +12,27 @@ public class AppUser {
     private Long id;
     private String name;
 
-    @Override
+    public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	@Override
     public String toString() {
         return String.format(
                 "AppUser[id=%d, name='%s']",
