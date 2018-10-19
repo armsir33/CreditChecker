d24947740d146aa3a6ccf1c59a50ed4b47bd9dd9
zni <ze.ni@worldline.com>
2018-10-19 15:14:08 +0200

    Add dynamic property file reloading

--- src/main/java/cn/credit/checker/CreditChecker/config/CreditCheckerConfig.java
+++ src/main/java/cn/credit/checker/CreditChecker/config/CreditCheckerConfig.java
@@ -4,15 +4,12 @@ import org.apache.commons.configuration2.FileBasedConfiguration;
 import org.apache.commons.configuration2.PropertiesConfiguration;
 import org.apache.commons.configuration2.builder.ReloadingFileBasedConfigurationBuilder;
 import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.commons.configuration2.reloading.PeriodicReloadingTrigger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.servlet.tags.Param;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.*;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class CreditCheckerConfig {
@@ -21,46 +18,33 @@ public class CreditCheckerConfig {
 
     private static final String PROPERTY_FILE_PATH = "application.properties";
 
-    private final PeriodicReloadingTrigger trigger;
+    private final ReloadingFileBasedConfigurationBuilder<PropertiesConfiguration> builder;
 
-    public CreditCheckerConfig(final ReloadingFileBasedConfigurationBuilder<FileBasedConfiguration> builder, final PeriodicReloadingTrigger trigger) {
-        this.trigger = trigger;
+    public CreditCheckerConfig(final ReloadingFileBasedConfigurationBuilder<PropertiesConfiguration> builder) {
+        this.builder = builder;
     }
 
-    public void start() {
+    public static CreditCheckerConfig create() {
+        LOG.debug("create CreditChecker");
         Parameters params = new Parameters();
         File propertiesFile = new File(PROPERTY_FILE_PATH);
 
-        ReloadingFileBasedConfigurationBuilder<FileBasedConfiguration> builder =
-                new ReloadingFileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
+        ReloadingFileBasedConfigurationBuilder<PropertiesConfiguration> builder =
+                new ReloadingFileBasedConfigurationBuilder<>(PropertiesConfiguration.class)
                         .configure(params.fileBased()
                                 .setFile(propertiesFile));
         PeriodicReloadingTrigger trigger = new PeriodicReloadingTrigger(builder.getReloadingController(),
-                null, 1, TimeUnit.MINUTES);
-        this.trigger.start();
-//        check();
+                null, 1, TimeUnit.SECONDS);
+        trigger.start();
+        return new CreditCheckerConfig(builder);
     }
 
-    public void check() {
-        ;
-//        while (true) {
-            /**
-             * 需要重新从 builder 中获取 Configuration 才会得到重新加载的配置项
-             */
-//            FileBasedConfiguration configuration = builder.getConfiguration();
-//            System.out.println(configuration.getProperty("key1"));
-//            try {
-//                Thread.sleep(2000);
-//            } catch (InterruptedException e) {
-//                LOG.warn("FAILED to sleep");
-//                new RuntimeException("FAILED to sleep", e);
-//            }
-//        }
+    public String getConfigProperty(String key) {
+        try {
+            return this.builder.getConfiguration().getString(key, "");
+        } catch (ConfigurationException e) {
+            throw new RuntimeException("FAILED to fetch configuration", e);
+        }
     }
-
-    public void stop() {
-        this.trigger.stop();
-    }
-
 
 }
